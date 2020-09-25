8e5dc56704e16eaa1edf3b5246cdb3ccf214f15c
adamnapieralski <adam.napieralski@gmail.com>
2020-09-25 16:46:26 +0200

    add openweathermap API key

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const appId = process.env.APPID || '38aef137bf77952448eb7348401e5306';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
