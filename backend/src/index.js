315a01da572f6310c16e67a18c4cdb82a50ee809
adamnapieralski <adam.napieralski@gmail.com>
2020-09-26 11:01:29 +0200

    eslint code fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,61 +1,64 @@
-const debug = require('debug')('weathermap');
+const debug = require('debug',)('weathermap',);
 
-const Koa = require('koa');
-const router = require('koa-router')();
-const fetch = require('node-fetch');
-const cors = require('kcors');
+const Koa = require('koa',);
+const router = require('koa-router',)();
+const fetch = require('node-fetch',);
+const cors = require('kcors',);
 
 const appId = process.env.APPID || '38aef137bf77952448eb7348401e5306';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
 const app = new Koa();
 
-app.use(cors());
+app.use(cors(),);
 
 const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
-  const response = await fetch(endpoint);
+  const response = await fetch(endpoint,);
 
-  return response ? response.json() : {}
+  debug('weather\nendpoint: %s\nresponse: %o', endpoint, response,);
+
+  return response ? response.json() : {};
 };
 
 const fetchForecast = async () => {
   const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
-  const response = await fetch(endpoint);
+  const response = await fetch(endpoint,);
 
-  return response ? response.json() : {}
-}
+  debug('forecast\nendpoint: %s\nresponse: %o', endpoint, response,);
 
-router.get('/api/weather', async ctx => {
+  return response ? response.json() : {};
+};
+
+router.get('/api/weather', async (ctx,) => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
-});
+},);
 
-router.get('/api/forecast', async ctx => {
+router.get('/api/forecast', async (ctx,) => {
   const forecastData = await fetchForecast();
 
   ctx.type = 'application/json; charset=utf-8';
   let body = forecastData.list
-    .map(el => ({
+    .map((el,) => ({
       weather: el.weather,
-      dt_txt: el.dt_txt
-    }));
-    
+      dt_txt: el.dt_txt,
+    }),);
   if (ctx.request.query.maxResults) {
-    body = body.filter((_, id) => id < ctx.request.query.maxResults);
+    body = body.filter((_, id,) => id < ctx.request.query.maxResults,);
   }
 
   ctx.body = body;
-});
+},);
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(router.routes(),);
+app.use(router.allowedMethods(),);
 
-app.listen(port);
+app.listen(port,);
 
-console.log(`App listening on port ${port}`);
+console.log(`App listening on port ${port}`,);
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,11 +4,14 @@ import Moment from 'react-moment';
 
 const baseURL = process.env.ENDPOINT;
 
+const forecastMaxResultsDay = 8;
+
 const getWeatherFromApi = async () => {
   try {
     const response = await fetch(`${baseURL}/weather`);
     return response.json();
   } catch (error) {
+    // FIXME
     console.error(error);
   }
 
@@ -20,69 +23,69 @@ const getForecastFromApi = async (maxResults) => {
     const response = await fetch(maxResults ? `${baseURL}/forecast?maxResults=${maxResults}` : `${baseURL}/forecast`);
     return response.json();
   } catch (error) {
+    // FIXME
     console.error(error);
   }
 
   return {};
-}
+};
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      icon: "",
-      forecastData: []
+      icon: '',
+      forecastData: [],
     };
   }
 
   async componentDidMount() {
     const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
+    this.setState({ icon: weather.icon.slice(0, -1) });
 
-    const forecast = await getForecastFromApi(8);
+    const forecast = await getForecastFromApi(forecastMaxResultsDay);
     this.setState({ forecastData: forecast });
   }
 
-  renderForecastCol(forecastData) {
-    return forecastData.map((data, id) => {
-      return (
-        <td key={id}>
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <Moment format="DD/MM">{data.dt_txt}</Moment>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <Moment format="HH:mm">{data.dt_txt}</Moment>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <img src={`/img/${data.weather[0].icon.slice(0, -1)}.svg`} />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      )
-    });
+  renderForecastCol() {
+    const { forecastData } = this.state;
+    return forecastData.map((data) => (
+      <td key={data.dt_txt}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <Moment format="DD/MM">{data.dt_txt}</Moment>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Moment format="HH:mm">{data.dt_txt}</Moment>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <img src={`/img/${data.weather[0].icon.slice(0, -1)}.svg`} alt={`Forecast icon at ${data.dt_txt}`} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    ));
   }
 
   render() {
-    const { icon, forecastData } = this.state;
+    const { icon } = this.state;
 
     return (
       <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+        { icon && <img src={`/img/${icon}.svg`} alt="Current weather icon" /> }
         <div>
           <table>
             <tbody>
               <tr>
-                { this.renderForecastCol(forecastData) }
+                { this.renderForecastCol() }
               </tr>
             </tbody>
           </table>
@@ -94,5 +97,5 @@ class Weather extends React.Component {
 
 ReactDOM.render(
   <Weather />,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
