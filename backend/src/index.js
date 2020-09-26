e94f919131c5bac3c8e4f4dc189326426ed6fcf6
adamnapieralski <adam.napieralski@gmail.com>
2020-09-26 09:10:46 +0200

    add get /api/forecast

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,11 +22,35 @@ const fetchWeather = async () => {
   return response ? response.json() : {}
 };
 
+const fetchForecast = async () => {
+  const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {}
+}
+
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get('/api/forecast', async ctx => {
+  const forecastData = await fetchForecast();
+
+  ctx.type = 'application/json; charset=utf-8';
+  let body = forecastData.list
+    .map(el => ({
+      weather: el.weather,
+      dt_txt: el.dt_txt
+    }));
+    
+  if (ctx.request.query.maxResults) {
+    body = body.filter((_, id) => id < ctx.request.query.maxResults);
+  }
+
+  ctx.body = body;
 });
 
 app.use(router.routes());
